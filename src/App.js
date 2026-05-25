c069b05a6930c2406c6888dcdc9ac526f1c8eee6
unknown <bmakhosazana26@gmail.com>
2026-05-25 16:44:02 +0200

    feat: added SheCodes Logo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <img src="/logo.png" className="App-logo" alt="logo" />
+
+    </div>
+  );
+}
+
+export default App;
